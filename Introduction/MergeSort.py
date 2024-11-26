9297a21225071bacdaf384faafd869bba01b8eb7
Anna Monso <anna.monso@estudiantat.upc.edu>
2024-11-26 15:08:45 +0100

    merge sort

--- Introduction/MergeSort.py
+++ Introduction/MergeSort.py
@@ -1,32 +1,40 @@
+def merge(v, p, q, r):
+    L = v[p:q+1]
+    R = v[q+1:r+1]
 
-def mergeSort(array):
-    if len(array) > 1:
-
-        mid = len(array)//2
-        L = array[:mid]
-        H = array[mid:]
-
-        mergeSort(L)
-        mergeSort(H)
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(H):
-            if L[i] <= H[j]:
-                array[k] = L[i]
-                i += 1
-            else:
-                array[k] = H[j]
-                j += 1
-            k += 1
-
-        while i < len(L):
-            array[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(H):
-            array[k] = H[j]
+    i=0 #i indexes the smallest remaining element in L
+    j=0 #j indexes the smallest remaining element in R
+    k=p
+    while i<len(L) and j<len(R):
+        if L[i]<=R[j]:
+            v[k] = L[i]  
+            i +=1
+        else :
+            v[k] = R[j]
             j += 1
-            k += 1
+        k +=1
+        #After the while loop either all of L or all of R has been copied back
+        #If it terminates because all of R has been copied, then j=len(R) but i<len(L) -> not all L has been copied back
+        #Thus, we require another loop to copy the remaining values of L into the last positions of v
 
+    while i<len(L):
+        v[k] = L[i]
+        i += 1
+        k += 1
+    while j<len(R):
+        v[k] = R[j]
+        j += 1
+        k +=1
+    return v
+
+
+def mergesort_rec(v, p, r):
+    if p>=r: return v #the array only contains one element. It is trivially sorted
+    if p<r: 
+        q = (p+r)//2
+        mergesort_rec(v,p,q)
+        mergesort_rec(v,q+1,r)
+        merge(v,p,q,r)  
+
+def mergesort(v): 
+  mergesort_rec(v, 0, len(v)-1)
